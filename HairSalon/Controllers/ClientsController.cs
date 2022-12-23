4bf43e42285499979b3f43b43a8aee811706f081
bodwood <bodiewood@hotmail.com>
2022-12-23 14:26:09 -0800

    add index method to view client list

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -16,5 +16,13 @@ namespace HairSalon.Controllers
     {
       _db = db;
     }
+
+    public ActionResult Index()
+    {
+      List<Client> model = _db.Clients
+                                .Include(client => client.Stylist)
+                                .ToList();
+      return View(model);
+    }
   }
 }
