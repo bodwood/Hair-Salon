c8deab7fef9ee92a2a0748fef00c162424a13706
bodwood <bodiewood@hotmail.com>
2022-12-23 09:52:04 -0800

    add ClientsController class that extends Controller library. A contrustor was added to allow for database connection

---
+++ HairSalon/Controllers/ClientsController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HairSalon.Controllers
+{
+  public class ClientsController : Controller
+  {
+
+    private readonly HairSalonContext _db;
+
+    public ClientsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+  }
+}
